4b86afc98294845a076116c825c160d0e72fca50
lalwanidj04 <147331633+lalwanidj04@users.noreply.github.com>
2024-03-26 21:58:45 +0530

    Add files via upload

---
+++ sol4/math.js
@@ -0,0 +1,17 @@
+function add(a,b){
+    const sum = a + b;
+    return sum;
+}
+function subtract(a,b){
+    const diff = a - b;
+    return diff;
+}
+function multiply(a,b){
+    const product = a*b;
+    return product;
+}
+function divide(a,b){
+    const quotient = a/b;
+    return quotient;
+}
+module.exports = {add,subtract,multiply,divide};
